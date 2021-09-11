72315f673a14447794bd7da0f567665d40db9bdd
unknown <peterjonlawrence@gmail.com>
2021-09-11 18:39:59 -0500

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,34 @@
 $(function () {
-    $(".carousel").carousel({ interval: 2000 });
-    $("#carouselButton").click(function () {
-        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-            $(".carousel").carousel("pause");
-            $("#carouselButton").children("i").removeClass("fa-pause");
-            $("#carouselButton").children("i").addClass("fa-play");
-        } else {
-            $(".carousel").carousel("cycle");
-            $("#carouselButton").children("i").removeClass("fa-play");
-            $("#carouselButton").children("i").addClass("fa-pause");
-        }
-    });
+	$(".carousel").carousel({ interval: 2000 });
+	$("#carouselButton").click(function () {
+		if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+			$(".carousel").carousel("pause");
+			$("#carouselButton").children("i").removeClass("fa-pause");
+			$("#carouselButton").children("i").addClass("fa-play");
+		} else {
+			$(".carousel").carousel("cycle");
+			$("#carouselButton").children("i").removeClass("fa-play");
+			$("#carouselButton").children("i").addClass("fa-pause");
+		}
+	});
+});
+
+$(function () {
+	$("#reserveButton").click(function () {
+		if ($("#reserveModal").modal("show")) {
+			$("#reserveModal").modal("hide");
+		} else {
+			$("#reserveModal").modal("show");
+		}
+	});
+});
+
+$(function () {
+	$("#loginButton").click(function () {
+		if ($("#loginModal").modal("show")) {
+			$("#loginModal").modal("hide");
+		} else {
+			$("#loginModal").modal("show");
+		}
+	});
 });
